8ea5d7cb276648e780cbfb2ed611f3550b57cd93
zhangjiale <zjl@MacBook-Pro-3.lan>
2020-07-18 09:57:14 +0800

    run

--- lib/provide/websocket.dart
+++ lib/provide/websocket.dart
@@ -7,35 +7,33 @@ import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/io.dart';
 import '../model/conversation.dart';
-class WebSocketProvide with ChangeNotifier{
+
+class WebSocketProvide with ChangeNotifier {
   var uid = '';
   var nickname = '';
   var users = [];
-  var groups =[];
-  var historyMessage = [];//接收到哦的所有的历史消息
+  var groups = [];
+  var historyMessage = []; //接收到哦的所有的历史消息
   var messageList = []; // 所有消息页面人员
-  var currentMessageList = [];//选择进入详情页的消息历史记录
-  var connecting = false;//websocket连接状态
+  var currentMessageList = []; //选择进入详情页的消息历史记录
+  var connecting = false; //websocket连接状态
   IOWebSocketChannel channel;
-  
-  
+
   init() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     final userInfo = prefs.getString('userInfo');
     print(userInfo);
-    if(userInfo == null){//弹出设置用户名
+    if (userInfo == null) {
+      //弹出设置用户名
       var now = new DateTime.now();
-      print(now.millisecondsSinceEpoch);//单位毫秒，13位时间戳
-      print(now.microsecondsSinceEpoch);//单位微秒,16位时间戳
+      print(now.millisecondsSinceEpoch); //单位毫秒，13位时间戳
+      print(now.microsecondsSinceEpoch); //单位微秒,16位时间戳
       uid = "flutter_${now.microsecondsSinceEpoch}";
       nickname = "flutter_${Random().nextInt(100)}";
-      var userInfoData = {
-        "uid" : uid,
-        "nickname" : nickname
-      };
-      var userInfoStr =  json.encode(userInfoData).toString();
+      var userInfoData = {"uid": uid, "nickname": nickname};
+      var userInfoStr = json.encode(userInfoData).toString();
       prefs.setString('userInfo', userInfoStr);
-    }else{
+    } else {
       var userInfoData = json.decode(userInfo.toString());
       uid = userInfoData['uid'];
       nickname = userInfoData['nickname'];
@@ -43,8 +41,10 @@ class WebSocketProvide with ChangeNotifier{
     return await createWebsocket();
     // monitorMessage();
   }
-  createWebsocket() async {//创建连接并且发送鉴别身份信息
-    channel = await new IOWebSocketChannel.connect('ws://111.231.225.178:3001');
+
+  createWebsocket() async {
+    //创建连接并且发送鉴别身份信息
+    channel = await new IOWebSocketChannel.connect('ws://192.168.199.177:3001');
     var obj = {
       "uid": uid,
       "type": 1,
@@ -56,68 +56,73 @@ class WebSocketProvide with ChangeNotifier{
     String text = json.encode(obj).toString();
     channel.sink.add(text);
     //监听到服务器返回消息
-    channel.stream.listen((data) => listenMessage(data),onError: onError,onDone: onDone);
+    channel.stream.listen((data) => listenMessage(data),
+        onError: onError, onDone: onDone);
   }
-    
-  listenMessage(data){
+
+  listenMessage(data) {
     connecting = true;
     var obj = jsonDecode(data);
     print(data);
-    if(obj['type'] == 1){ // 获取聊天室的人员与群列表
+    if (obj['type'] == 1) {
+      // 获取聊天室的人员与群列表
       messageList = [];
       print(obj['msg']);
       users = obj['users'];
       groups = obj['groups'];
-      for(var i = 0; i < groups.length; i++){
+      for (var i = 0; i < groups.length; i++) {
         messageList.add(new Conversation(
-          avatar: 'assets/images/ic_group_chat.png',
-          title: groups[i]['name'],
-          des: '点击进入聊天',
-          updateAt: obj['date'].substring(11,16),
-          unreadMsgCount: 0,
-          displayDot: false,
-          groupId: groups[i]['id'],
-          type: 2
-        ));
-      }
-      for(var i = 0; i < users.length; i++){
-        if(users[i]['uid'] != uid){
-          messageList.add(new Conversation(
             avatar: 'assets/images/ic_group_chat.png',
-            title: users[i]['nickname'],
+            title: groups[i]['name'],
             des: '点击进入聊天',
-            updateAt: obj['date'].substring(11,16),
+            updateAt: obj['date'].substring(11, 16),
             unreadMsgCount: 0,
             displayDot: false,
-            userId:users[i]['uid'],
-            type: 1
-          ));
+            groupId: groups[i]['id'],
+            type: 2));
+      }
+      for (var i = 0; i < users.length; i++) {
+        if (users[i]['uid'] != uid) {
+          messageList.add(new Conversation(
+              avatar: 'assets/images/ic_group_chat.png',
+              title: users[i]['nickname'],
+              des: '点击进入聊天',
+              updateAt: obj['date'].substring(11, 16),
+              unreadMsgCount: 0,
+              displayDot: false,
+              userId: users[i]['uid'],
+              type: 1));
         }
       }
-    }else if (obj['type'] == 2){//接收到消息
+    } else if (obj['type'] == 2) {
+      //接收到消息
       historyMessage.add(obj);
       print(historyMessage);
-      for(var i = 0; i < messageList.length; i++){
-        if(messageList[i].userId != null){
+      for (var i = 0; i < messageList.length; i++) {
+        if (messageList[i].userId != null) {
           var count = 0;
-          for(var r = 0; r < historyMessage.length; r++){
-            if(historyMessage[r]['status']==1 &&  historyMessage[r]['bridge'].contains(messageList[i].userId) && historyMessage[r]['uid'] != uid){
+          for (var r = 0; r < historyMessage.length; r++) {
+            if (historyMessage[r]['status'] == 1 &&
+                historyMessage[r]['bridge'].contains(messageList[i].userId) &&
+                historyMessage[r]['uid'] != uid) {
               count++;
             }
           }
-          if(count > 0){
+          if (count > 0) {
             messageList[i].displayDot = true;
             messageList[i].unreadMsgCount = count;
           }
         }
-        if(messageList[i].groupId != null){
+        if (messageList[i].groupId != null) {
           var count = 0;
-          for(var r = 0; r < historyMessage.length; r++){
-            if(historyMessage[r]['status']==1 &&  historyMessage[r]['groupId']==messageList[i].groupId && historyMessage[r]['uid'] != uid){
+          for (var r = 0; r < historyMessage.length; r++) {
+            if (historyMessage[r]['status'] == 1 &&
+                historyMessage[r]['groupId'] == messageList[i].groupId &&
+                historyMessage[r]['uid'] != uid) {
               count++;
             }
           }
-          if(count > 0){
+          if (count > 0) {
             messageList[i].displayDot = true;
             messageList[i].unreadMsgCount = count;
           }
@@ -126,15 +131,17 @@ class WebSocketProvide with ChangeNotifier{
     }
     notifyListeners();
   }
-  sendMessage(type,data,index){//发送消息
+
+  sendMessage(type, data, index) {
+    //发送消息
     print(messageList[index].userId);
     print(messageList[index].groupId);
     var _bridge = [];
-    if(messageList[index].userId != null){
+    if (messageList[index].userId != null) {
       _bridge..add(messageList[index].userId)..add(uid);
     }
     int _groupId;
-    if(messageList[index].groupId != null){
+    if (messageList[index].groupId != null) {
       _groupId = messageList[index].groupId;
     }
     print(_bridge);
@@ -143,14 +150,15 @@ class WebSocketProvide with ChangeNotifier{
       "type": 2,
       "nickname": nickname,
       "msg": data,
-      "bridge": _bridge ,
+      "bridge": _bridge,
       "groupId": _groupId
     };
     String text = json.encode(obj).toString();
     print(text);
     channel.sink.add(text);
   }
-  onError(error){
+
+  onError(error) {
     print('error------------>${error}');
   }
 
@@ -160,7 +168,8 @@ class WebSocketProvide with ChangeNotifier{
     print('websocket重连');
   }
 
-  closeWebSocket(){//关闭链接
+  closeWebSocket() {
+    //关闭链接
     channel.sink.close();
     print('关闭了websocket');
     notifyListeners();
